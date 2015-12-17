fcf414c6f712532abfebb0430e1a217c2abd6115
inkblotty <inkblot.and.blush@gmail.com>
2015-12-16 19:45:02 -0700

    completed 9_timeServer and created 10_httpFileServer

---
+++ 10_httpFileServer.js
@@ -0,0 +1,11 @@
+// HTTP FILE SERVER
+
+var http = require('http');
+var fs = require('fs');
+var port = process.argv[2];
+
+var server = http.createServer(function(req, res){
+
+});
+
+server.listen(port);
--- 9_timeServer.js
+++ 9_timeServer.js
@@ -1,2 +1,15 @@
 // TIME SERVER
 
+var net = require('net');
+var port = process.argv[2];
+
+var server = net.createServer(function(socket){
+	var date = new Date();
+	var formattedDate = date.getFullYear() + "-" + (Number(date.getMonth())+1).toString() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
+	console.log(formattedDate + '\n');
+
+	socket.write(formattedDate);
+	socket.end();
+});
+
+server.listen(port);
