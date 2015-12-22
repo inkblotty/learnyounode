ac68701dd4dac067079477d448f9578347c4361c
inkblotty <inkblot.and.blush@gmail.com>
2015-12-21 23:11:32 -0700

    finished 11_httpUppercaserer

--- 11_httpUppercaserer.js
+++ 11_httpUppercaserer.js
@@ -1,17 +1,13 @@
 // HTTP UPPERCASERER
 
 var http = require('http');
-var fs = require('fs');
 var map = require('through2-map');
-
 var port = require(process.argv[2]);
 
 var server = http.createServer(function(req, res){
-	var outStream;
-
-	res.pipe(map(function(chunk){
-		chunk.toString().uppercase()
-	})).pipe(outStream);
+	req.pipe(map(function(chunk){
+		return chunk.toString().toUpperCase()
+	})).pipe(res);
 });
 
 server.listen(port);
