d1d00943c74a32271db89436dc39658c3ff7af1a
inkblotty <inkblot.and.blush@gmail.com>
2015-12-21 22:42:03 -0700

    added solution to 10_httpFileServer

--- 10_httpFileServer.js
+++ 10_httpFileServer.js
@@ -2,10 +2,26 @@
 
 var http = require('http');
 var fs = require('fs');
+
 var port = process.argv[2];
+var file = process.argv[3];
 
 var server = http.createServer(function(req, res){
-	fs.createReadStream(res);
+	var src = fs.createReadStream(file);
+	src.pipe(res);
 });
 
 server.listen(port);
+
+/* official solution:
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+	res.writeHead(200, { 'content-type': 'text/plain' })
+	fs.createReadStream(process.argv[3]).pipe(res);
+});
+
+server.listen(Number(process.argv[2]));
+*/
